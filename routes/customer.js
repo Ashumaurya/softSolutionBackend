539a39d2d736d10d5614c3ebf2b3d07778643324
Ashish maurya <50199029+Ashumaurya@users.noreply.github.com>
2021-07-28 14:44:59 +0530

    Customer Validation are working properly

--- routes/customer.js
+++ routes/customer.js
@@ -10,6 +10,10 @@ router.post(
     .isLength({ min: 2 })
     .withMessage("first Name must be atleast 2 char"),
   check("email").isEmail().withMessage("Please enter a valid email"),
+  check("phone")
+    .isInt()
+    .isLength({ min: 10, max: 10 })
+    .withMessage("Enter a valid Phone Number"),
   check("email").custom((value) => {
     return Customer.findOne({ email: value }).then((customer) => {
       if (customer) {
